2cb2e1ed8cd3ac9c2221c4b0d0e722579776a743
mkaranja <m.karanja@cgiar.org>
2024-03-28 09:22:01 +0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -7,9 +7,9 @@ library(quadrangle)
 
 fluidPage(
   useShinyjs(),
-  tags$video(id = "video", width = 300, height = 300),
-  tags$canvas(id = "canvas", width = 300, height = 300),
+  tags$video(id = "video", width = 300, height = 300) |> hidden(),
+  tags$canvas(id = "canvas", width = 300, height = 300) |> hidden(),
   actionButton("scan", "Scan QR Code"),
-  verbatimTextOutput("result"),
-  shiny::tags$a(href="https://github.com/mkaranja/shinyquadrangle", target="_blank", "code")
+  verbatimTextOutput(("result2")),
+  shiny::tags$a(href="https://github.com/mkaranja/shinyquadrangle", target="_blank", "Source code in github")
 )
