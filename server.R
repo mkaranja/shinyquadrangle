67dd3b124474eff8b60856e57cfc3e7a4fd855c2
mkaranja <m.karanja@cgiar.org>
2024-03-28 09:22:31 +0300

    Update server.R

--- server.R
+++ server.R
@@ -11,8 +11,10 @@ library(base64enc)
 library(magick)
 library(quadrangle)
 
-function(input, output) {
+function(input, output, session) {
   observeEvent(input$scan, {
+    shinyjs::show("video")
+
     runjs("
       navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } }).then(function(stream) {
         var video = document.getElementById('video');
@@ -24,27 +26,56 @@ function(input, output) {
           context.drawImage(video, 0, 0, 300, 300);
           var imageData = context.getImageData(0, 0, 300, 300);
           var base64Image = canvas.toDataURL('image/png');
-          Shiny.onInputChange('imageData', base64Image);
-        }, 500); // Reduced refresh rate
+          Shiny.setInputValue('imageData', base64Image);
+        }, 500); // Increased interval for better performance
       }).catch(function(err) {
         console.error('Error accessing camera: ', err);
       });
     ")
   })
 
-  output$result <- renderPrint({
+  qr_scan_result <- reactive({
     req(input$imageData)
     imageData <- input$imageData
-    # Remove the "data:image/png;base64," prefix from the base64 image data
-    imageData <- gsub("data:image/png;base64,", "", imageData)
-    # Convert the base64 image data to binary
-    binaryImageData <- base64decode(imageData)
+    if (!is.null(imageData)) {
 
-    # Read the binary image data into an image object
-    magickImage <- image_read(binaryImageData)
+      # Remove the "data:image/png;base64," prefix from the base64 image data
+      imageData <- gsub("data:image/png;base64,", "", imageData)
+      # Convert the base64 image data to binary
+      binaryImageData <- base64decode(imageData)
 
-    # Decode the QR code using qr_decode()
-    result <- try(quadrangle::qr_scan(magickImage)$values$value)
-    result
+      # Read the binary image data into an image object
+      magickImage <- image_read(binaryImageData)
+
+      # Decode the QR code using qr_decode()
+      try(quadrangle::qr_scan(magickImage)$values$value, silent = TRUE)
+    }
   })
+
+  observeEvent( qr_scan_result() , {
+    if(length(qr_scan_result())>0){
+      #updateTextInput(session, "result", "Identity", value = qr_scan_result())
+      stopVideoStream()
+      shinyjs::hide("video")
+    }
+  })
+
+
+  output$result2 <- renderPrint({
+    qr_scan_result()
+  })
+
+  stopVideoStream <- function() {
+    runjs("
+      var video = document.getElementById('video');
+      var stream = video.srcObject;
+      if (stream) {
+        var tracks = stream.getTracks();
+        tracks.forEach(function(track) {
+          track.stop();
+        });
+        video.srcObject = null;
+      }
+    ")
+  }
 }
